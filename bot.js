7392bfc62077d9a73e82d7db3eb4eabb79c8550c
botMother <dsfox@yandex.ru>
2016-05-23 13:32:41 +0300

    telegram bot core

---
+++ bot.js
@@ -0,0 +1,27 @@
+var AutoBot = require("node-telegram-bot-api");
+var fs = require("fs");
+var token = fs.readFileSync("token.key");
+var botOptions = {
+    polling: true
+};
+var bot = new AutoBot(token, botOptions);
+bot.getMe().then(function(me) {
+    console.log("Hello! My name is %s!", me.first_name);
+    console.log("My id is %s.", me.id);
+    console.log("And my username is @%s.", me.username);
+});
+bot.on("text", function(msg) {
+    var cid = msg.chat.id;
+    var text = msg.text;
+    var date = msg.date;
+    var uid = msg.from.uid;
+
+    if (messageText === "/test") {
+        sendMessage(cid, "thx! I'm fine");
+    }
+    console.log(msg);
+});
+
+function sendMessage(aChatId, aMessage) {
+    bot.sendMessage(aChatId, aMessage);
+}
